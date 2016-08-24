c81e576b61449c0bfbacbaf107bc201ad0c4b79c
Weston Ruter <weston@xwp.co>
2016-08-24 14:15:03 -0700

    Eliminate edit post links in notifications per discussion

--- src/wp-admin/js/customize-posts.js
+++ src/wp-admin/js/customize-posts.js
@@ -56,27 +56,6 @@
 
 		api.bind( 'saved', function() {
 
-			// Show users links to edit newly-published posts.
-			api.control.each( function( control ) {
-				var id, url, message;
-				if ( 'nav_menu_item' === control.params.type ) {
-					if ( -1 !== component.autoDrafts.indexOf( control.setting().object_id ) ) {
-						id = component.autoDrafts[component.autoDrafts.indexOf( control.setting().object_id )];
-						url = api.Menus.data.editPostURL.replace( '%d', id );
-						message = api.Menus.data.l10n.newPostPublished.replace( '%3$s', url );
-						message = message.replace( '%1$s', control.params.item_type_label );
-						message = message.replace( '%2$s', control.setting().original_title );
-						control.setting.notifications.add( 'content_published', new wp.customize.Notification(
-							'content_published',
-							{
-								type: 'info',
-								message: message
-							}
-						) );
-					}
-				}
-			});
-
 			// Reset auto-drafts.
 			component.autoDrafts = []; // Reset the array the next time an item is created. Don't update the setting yet as that would trigger the customizer's dirty state.
 		} );
