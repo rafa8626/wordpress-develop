4a9f0797e5fd7be49a8d66c0e3db06dd2a36c4a1
Weston Ruter <weston@xwp.co>
2016-08-25 18:15:24 -0700

    Ensure user can edit_post specifically beyond just publish_posts

--- src/wp-includes/class-wp-customize-nav-menus.php
+++ src/wp-includes/class-wp-customize-nav-menus.php
@@ -1051,7 +1051,7 @@ final class WP_Customize_Nav_Menus {
 			if ( ! $post_type_obj ) {
 				continue;
 			}
-			if ( ! current_user_can( $post_type_obj->cap->publish_posts ) ) {
+			if ( ! current_user_can( $post_type_obj->cap->publish_posts ) || ! current_user_can( $post_type_obj->cap->edit_post, $post_id ) ) {
 				continue;
 			}
 			$post_ids[] = $post->ID;
