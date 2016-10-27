0f9742b9d68285e88df8e52df2f83a3a60242a48
Helen Hou-Sandi <helen@git.wordpress.org>
2016-10-27 16:27:46 -0700

    https://core.trac.wordpress.org/attachment/ticket/38114/38114.2.diff

--- src/wp-includes/theme.php
+++ src/wp-includes/theme.php
@@ -1623,6 +1623,140 @@ function get_editor_stylesheets() {
 }
 
 /**
+ * Expand a theme's starter content configuration using core-provided data.
+ *
+ * @since 4.7.0
+ *
+ * @return array Array of starter content.
+ */
+function get_theme_starter_content() {
+	$config = get_theme_support( 'starter-content' );
+
+	if ( empty( $config ) ) {
+		return false;
+	}
+
+	$core_content = array (
+		'sidebars_widgets' => array (
+			'text_business_info' => array ( 'text', array (
+				'title' => __( 'Find Us' ),
+				'text' => join( '', array (
+					'<p><strong>' . __( 'Address' ) . '</strong><br />',
+					__( '123 Main Street' ) . '<br />' . __( 'New York, NY 10001' ) . '</p>',
+					'<p><strong>' . __( 'Hours' ) . '</strong><br />',
+					__( 'Monday&mdash;Friday: 9:00AM&ndash;5:00PM' ) . '<br />' . __( 'Saturday &amp; Sunday: 11:00AM&ndash;3:00PM' ) . '</p>'
+				) ),
+			) ),
+			'search' => array ( 'search', array (
+				'title' => __( 'Site Search' ),
+			) ),
+			'text_credits' => array ( 'text', array (
+				'title' => __( 'Site Credits' ),
+				'text' => sprintf( __( 'This site was created on %s' ), get_date_from_gmt( current_time( 'mysql', 1 ), 'c' ) ),
+			) ),
+		),
+		'nav_menus' => array (
+			'link_yelp' => 'https://www.yelp.com',
+			'link_facebook' => 'https://www.facebook.com/wordpress',
+			'link_twitter' => 'https://twitter.com/wordpress',
+			'link_instagram' => 'https://www.instagram.com/explore/tags/wordcamp/',
+			'link_email' => 'mailto:wordpress@example.com',
+		),
+		'posts' => array(
+			'home' => array(
+				'post_type' => 'page',
+				'post_title' => __( 'Homepage' ),
+				'post_content' => __( 'Welcome home.' ),
+			),
+			'about-us' => array(
+				'post_type' => 'page',
+				'post_title' => __( 'About Us' ),
+				'post_content' => __( 'More than you ever wanted to know.' ),
+			),
+			'contact-us' => array(
+				'post_type' => 'page',
+				'post_title' => __( 'Contact Us' ),
+				'post_content' => __( 'Call us at 999-999-9999.' ),
+			),
+			'blog' => array(
+				'post_type' => 'page',
+				'post_title' => __( 'Blog' ),
+			),
+
+			'homepage-section' => array(
+				'post_type' => 'page',
+				'post_title' => __( 'A homepage section' ),
+				'post_content' => __( 'This is an example of a homepage section, which are managed in theme options.' ),
+			),
+		),
+	);
+
+	$content = array();
+
+	foreach ( $config as $type => $args ) {
+		switch( $type ) {
+			// Use options and theme_mods as-is
+			case 'options' :
+			case 'theme_modes' :
+				$content[ $type ] = $config[ $type ];
+				break;
+
+			// Widgets are an extra level down due to groupings
+			case 'sidebars_widgets' :
+				foreach( $config[ $type ] as $group => $items ) {
+					foreach ( $items as $id => $item ) {
+						$id = $item[1];
+						if ( ! empty( $core_content[ $type ] && ! empty( $core_content[ $type ][ $id ] ) ) ) {
+							$content[ $type ][ $group ][ $id ][] = $item[0];
+							$content[ $type ][ $group ][ $id ][] = $core_content[ $type ][ $id ];
+						}
+					}
+				}
+				break;
+
+			// And nav menus are yet another level down 
+			case 'nav_menus' :
+				foreach( $config[ $type ] as $group => $args ) {
+					// Menu groups need a name
+					if ( empty( $args['name'] ) ) {
+						$args['name'] = $group;
+					}
+
+					$content[ $type ][ $group ]['name'] = $args['name'];
+
+					// Do we need to check if this is empty?
+					foreach ( $args['items'] as $id ) {
+						if ( ! empty( $core_content[ $type ] && ! empty( $core_content[ $type ][ $id ] ) ) ) {
+							$content[ $type ][ $group ]['items'][ $id ] = $core_content[ $type ][ $id ];
+						}
+					}
+				}
+				break;
+
+
+			// Everything else should map at the next level
+			default :
+				foreach( $config[ $type ] as $id ) {
+					if ( ! empty( $core_content[ $type ] && ! empty( $core_content[ $type ][ $id ] ) ) ) {
+						$content[ $type ][ $id ] = $core_content[ $type ][ $id ];
+					}
+				}
+				break;
+		}
+	}
+
+	/**
+	 * Filters the expanded array of starter content.
+	 *
+	 * @since 4.7.0
+	 *
+	 * @param array $content Array of starter content.
+	 * @param array $config  Array of theme-specific starter content configuration.
+	 */
+	return apply_filters( 'get_theme_starter_content', $content, $config );
+}
+
+/**
  * Registers theme support for a given feature.
  *
  * Must be called in the theme's functions.php file to work.
@@ -1634,12 +1768,13 @@ function get_editor_stylesheets() {
  * @since 3.9.0 The `html5` feature now also accepts 'gallery' and 'caption'
  * @since 4.1.0 The `title-tag` feature was added
  * @since 4.5.0 The `customize-selective-refresh-widgets` feature was added
+ * @since 4.7.0 The `starter-content` feature was added
  *
  * @global array $_wp_theme_features
  *
  * @param string $feature  The feature being added. Likely core values include 'post-formats',
  *                         'post-thumbnails', 'html5', 'custom-logo', 'custom-header-uploads',
- *                         'custom-header', 'custom-background', 'title-tag', etc.
+ *                         'custom-header', 'custom-background', 'title-tag', 'starter-content', etc.
  * @param mixed  $args,... Optional extra arguments to pass along with certain features.
  * @return void|bool False on failure, void otherwise.
  */
@@ -2070,7 +2205,8 @@ function current_theme_supports( $feature ) {
 	 *
 	 * The dynamic portion of the hook name, `$feature`, refers to the specific theme
 	 * feature. Possible values include 'post-formats', 'post-thumbnails', 'custom-background',
-	 * 'custom-header', 'menus', 'automatic-feed-links', 'html5', and `customize-selective-refresh-widgets`.
+	 * 'custom-header', 'menus', 'automatic-feed-links', 'html5',
+	 * 'starter-content', and 'customize-selective-refresh-widgets'.
 	 *
 	 * @since 3.4.0
 	 *
