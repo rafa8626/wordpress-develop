25339cffe6a89aaa5fb1572515d15b2dfd977fd5
Payton Swick <payton@foolord.com>
2016-10-27 20:11:59 -0400

    Switch to using inline svg for edit shortcut

--- src/wp-includes/js/customize-selective-refresh.js
+++ src/wp-includes/js/customize-selective-refresh.js
@@ -214,7 +214,7 @@ wp.customize.selectiveRefresh = ( function( $, api ) {
 		 * @return {jQuery} The edit shortcut button element.
 		 */
 		createEditShortcut: function() {
-			var partial = this, shortcutTitle, $button, $clickTarget;
+			var partial = this, shortcutTitle, $button, $clickTarget, $image;
 			shortcutTitle = partial.getEditShortcutTitle();
 			$button = $( '<span>', {
 				'class': 'customize-partial-edit-shortcut ' + partial.getEditShortcutClassName()
@@ -222,8 +222,10 @@ wp.customize.selectiveRefresh = ( function( $, api ) {
 			$clickTarget = $( '<button>', {
 				'aria-label': shortcutTitle,
 				'title': shortcutTitle,
-				'class': 'customize-partial-edit-shortcut-click-target dashicons dashicons-edit',
-			} )
+				'class': 'customize-partial-edit-shortcut-click-target',
+			} );
+			$image = $( '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M13.89 3.39l2.71 2.72c.46.46.42 1.24.03 1.64l-8.01 8.02-5.56 1.16 1.16-5.58s7.6-7.63 7.99-8.03c.39-.39 1.22-.39 1.68.07zm-2.73 2.79l-5.59 5.61 1.11 1.11 5.54-5.65zm-2.97 8.23l5.58-5.6-1.07-1.08-5.59 5.6z"/></svg>' );
+			$clickTarget.append( $image );
 			$button.append( $clickTarget );
 			return $button;
 		},
