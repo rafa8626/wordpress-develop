bd47c42bd5b0f726f331622ba3767af44fb2c0d3
Justin Kopepasah <justin@kopepasah.com>
2017-05-09 13:55:28 +0900

    36163 Allow empty array to allow not classes added.
    If class names are returned as an empty array, let's not append `class=` to the `ul` element.

--- src/wp-includes/class-walker-nav-menu.php
+++ src/wp-includes/class-walker-nav-menu.php
@@ -70,10 +70,10 @@ class Walker_Nav_Menu extends Walker {
 		 */
 
 		$classes = array( 'sub-menu' );
-		$classes = apply_filters( 'nav_menu_submenu_css_class', $classes, $args, $depth );
-		$class_names = join( ' ', $classes );
+		$class_names = join( ' ', apply_filters( 'nav_menu_submenu_css_class', $classes, $args, $depth ) );
+		$class_names = $class_names ? ' class="' . esc_attr( $class_names ) . '"' : '';
 
-		$output .= "{$n}{$indent}<ul class='" . esc_attr( $class_names ) . "'>{$n}";
+		$output .= "{$n}{$indent}<ul $class_names>{$n}";
 	}
 
 	/**
